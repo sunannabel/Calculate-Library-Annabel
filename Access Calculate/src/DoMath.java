de9b810efdf1aa059d13f20d3e0753203f970504
APCS2nd <APCS2nd@MHS-Math2-25>
2018-09-06 09:50:24 -0700

    everything written in class 9/6 basically setup

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*This is the client code for the Calculate Library
+ * @author Annabel Sun
+ * @version September 6, 2018
+ */
+
+
+public class DoMath {
+	public static void main(String[] args) (
+		//auto generated whatever idk
+	
+			
+	)
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,10 @@
+/* This class contains the methods to do various math related tasks.
+ * @author Annabel Sun
+ * @version September 6 2018
+ */
+public class Calculate {
+	
+	public int square(int number) (
+			return number;
+	)
+}
