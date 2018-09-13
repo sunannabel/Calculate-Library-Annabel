83455addd5c47e21b4b3a2dac5473622267c1bc1
APCS2nd <APCS2nd@MHS-Math2-25>
2018-09-13 09:51:52 -0700

    coded all of part 1

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,5 +7,15 @@
 public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(-3)); 	
+		System.out.println(Calculate.cube(-3));
+		System.out.println(Calculate.average(2.0, 4.0));
+		System.out.println(Calculate.average(1.0, 3.0, 5.0));
+		System.out.println(Calculate.toDegrees(2.0));
+		System.out.println(Calculate.toRadians(180));
+		System.out.println(Calculate.discriminant(2.0, 3.0, 4.0));
+		System.out.println(Calculate.toImproperFrac(4, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
 	}
 }
+	
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -3,7 +3,50 @@
  * @version September 12 2018
  */
 public class Calculate {
-	public int square(int number) {
-			return number*number; //returns the square of an int
+	public static int square (int number) {
+		return number*number; //returns the square of an int
 	}
+	
+	public static int cube (int number) {
+		return number*number*number; //returns the cube of an int
+	}
+	
+	public static double average (double num1, double num2) {
+		return (num1+num2)/2;
+	}
+	
+	public static double average (double num1, double num2, double num3) {
+		return (num1+num2+num3)/3;
+	}
+	
+	public static double toDegrees (double number) {
+		return (number*180)/3.14159;
+	}
+	
+	public static double toRadians (double number) {
+		return (number*3.14159)/180;
+	}
+	
+	public static double discriminant (double a, double b, double c) {
+		return (b*b) - (4*a*c);
+	}
+	
+	public static String toImproperFrac (int whole, int numerator, int denominator) {
+		int x = whole*denominator + numerator;
+		return (x+"/"+denominator);
+	}
+	
+	public static String toMixedNum (int numerator, int denominator) {
+		int x = numerator/denominator;
+		int y = numerator%denominator;
+		return (x+"_"+y+"/"+denominator);
+	}
+	
+	public static String foil (int a, int b, int c, int d, String var) {
+		int x = a*c; //ax^2
+		int y = a*d + b*c; //bx
+		int z = b*d; //c
+		return (x + var + "^2 + " + y + var + " + " + z);
+	}
+	
 }
