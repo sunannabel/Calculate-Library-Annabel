e3cb8fd8ce86f361cb2446ebb35a011cba162ddc
Annabel <Annabel@10.236.202.135>
2018-09-16 16:59:35 -0700

    finished part 2 and part 3 methods!

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -16,6 +16,17 @@ public class DoMath {
 		System.out.println(Calculate.toImproperFrac(4, 1, 2));
 		System.out.println(Calculate.toMixedNum(7, 2));
 		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+		System.out.println(Calculate.isDivisibleBy(5, 10));
+		System.out.println(Calculate.absValue(-9.0));
+		System.out.println(Calculate.max(3.0, 2.0));
+		System.out.println(Calculate.max(7.0, 9.0, 8.0)); //num2;  9.0
+		System.out.println(Calculate.min(1, 2)); //return 1
+		System.out.println(Calculate.round2(1.235));
+		System.out.println(Calculate.exponent(2.0, 4));
+		System.out.println(Calculate.factorial(4));
+		System.out.println(Calculate.isPrime(223));
+		System.out.println(Calculate.gcf(18,24));
+		System.out.println(Calculate.sqrt(1.5625));
 	}
 }
 	
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -49,4 +49,106 @@ public class Calculate {
 		return (x + var + "^2 + " + y + var + " + " + z);
 	}
 	
+	public static boolean isDivisibleBy (int num1, int num2) {
+		if (num1 > num2) {
+			if (num1 % num2 == 0) {
+				return true;
+			} else {
+				return false;
+			} 
+		} else if (num2 > num1) {
+			if (num2 % num1 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return true;
+		}
+	}
+	
+	public static double absValue (double number) {
+		if (number < 0) {
+			return number * -1;
+		} else {
+			return number;
+		}
+	}
+	
+	public static double max (double num1, double num2) {
+		if (num1 > num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	public static double max (double num1, double num2, double num3) {
+		double x = max(num1, max(num2, num3));
+		return x;
+	}
+	
+	public static int min (int num1, int num2) {
+		if (num1 < num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	public static double round2(double number) {
+		double a = number * 1000; // example: 1.235 -> 1235
+		double b = a % 10; //5
+		if (b >= 5) { //true
+			int c = (int) (a+5)/10; //124
+			return c/100.0; //1.24
+		} else {
+			int d = (int) a/10;
+			return d/100.0;
+		}
+	}
+	
+	public static double exponent(double num1, int num2) {
+		double num3 = 1.0;
+		for (int i = 1; i <=num2; i++) {
+			num3 = num3*num1;
+		}
+		return num3;
+	}
+	
+	public static int factorial(int number) {
+		int num2 = 1;
+		for (int i = number; i >= 2; i--) {
+			num2 = num2*i;
+		}
+		return num2;
+	}
+	
+	public static boolean isPrime(int number) {
+		for (int i = 2; i < number/2; i++) {
+			if(isDivisibleBy(number, i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static int gcf(int num1, int num2) {
+		int x = 1;
+		for (int i = 1; i < num1; i++) {
+			if (isDivisibleBy(num1, i) && isDivisibleBy(num2, i) ) {
+				x = i;
+			}
+		}
+		return x;
+	}
+	
+	public static double sqrt(double number) { 
+		double guess = 0.1;
+		while (absValue(number - guess*guess) > 0.005) { 
+			guess = (number/guess + guess)/2; 
+		}
+		return round2(guess);
+	}
+	
 }
